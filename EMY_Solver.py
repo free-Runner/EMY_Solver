699587cc5e09965b996f46b973684911df9878f8
free-Runner <aali07@students.poly.edu>
2014-03-16 02:38:40 -0400

    Forwarding, stalls added to work with input

--- EMY_Solver.py
+++ EMY_Solver.py
@@ -5,146 +5,202 @@
 #   Currently implementing v2: Int + FP pipeline
 
 class Register:
-	def __init__(self,name):
-		self.name = name
-	
+    def __init__(self,name):
+        self.name = name
+
+class Stage:
+    def __init__(self):
+        self.stall = None
+        self.start = None
+        self.end = None
+
+    def __str__(self):
+        if self.end:
+            if self.stall:
+                return "%s/%s-%s" %(self.start,self.stall,self.end)
+            else:
+                if self.start != self.end:
+                    return "%s-%s" %(self.start,self.end)
+                else:
+                    return "%s" %self.start
+        else:
+            if self.stall:
+                return "%s/%s" %(self.start,self.stall)
+            else:
+                return "%s" %self.start
+
+    
 class Instruction:
-	def __init__(self,opcode,instr):
-		self.i = instr
-		self.opcode = opcode
-		# Src & output regs
-		self.rs = None
-		self.ro = None
-		self.latency = 1
-		# Define stages
-		self.IF = None
-		self.ID = None
-		self.EX = None
-		self.MEM = None
-		self.CM = None
+    def __init__(self,opcode,instr):
+        self.i = instr
+        self.opcode = opcode
+        # Src & output regs
+        self.rs = None
+        self.ro = None
+        self.latency = 1
+        # Define stages
+        self.IF = Stage()
+        self.ID = Stage()
+        self.EX = Stage()
+        self.MEM = Stage()
+        self.CM = Stage()
 
-	def compute_regs(self,regs):
-		oc = self.opcode
-		line = regs.split(', ')
-		self.ro = line[0]
-		
-		# loads
-		if oc in ['L.D','LD','SD','S.D']:
-			self.rs = line[1].split('(')[1][:-1]
-			
-		# calculations
-		if oc in ['MUL.D','ADD.D','SUB.D','DIV.D']:
-			self.latency = 3
-			self.rs = list(set(line[1:]))
+    def compute_regs(self,regs):
+        oc = self.opcode
+        line = regs.split(', ')
+        self.ro = line[0]
+        
+        # loads
+        if oc in ['L.D','LD']:
+            self.rs = [line[1].split('(')[1][:-1]]
 
-		# immediate calculations
-		if oc in ['DADDI','MULDI']:
-			self.rs = line[1]
-			
-		# branches
-		if oc in ['BNEZ']:
-			self.rs = self.ro
-			self.ro = None
+        # stores
+        if oc in ['SD','S.D']:
+            self.r_mem = line[0]
+            self.rs = line[1].split('(')[1][:-1]
+            
+        # calculations
+        if oc in ['MUL.D','ADD.D','SUB.D','DIV.D']:
+            self.latency = 3
+            self.rs = list(set(line[1:]))
+
+        # immediate calculations
+        if oc in ['DADDI','MULDI']:
+            self.rs = line[1]
+            
+        # branches
+        if oc in ['BNEZ']:
+            self.rs = self.ro
+            self.ro = None
 
 def create_instructions(data):
-	i_list = []
-	for instr in data:
-		instr = instr.strip()
-		oc, regs = instr.split('\t')
-		i = Instruction(oc,instr)
-		i.compute_regs(regs)
-		i_list.append(i)
-		
-	return i_list
+    i_list = []
+    for instr in data:
+        instr = instr.strip()
+        oc, regs = instr.split('\t')
+        i = Instruction(oc,instr)
+        i.compute_regs(regs)
+        i_list.append(i)
+        
+    return i_list
 
 def display(i_list):
-	
-	for instr in i_list:
-		print instr.i,'\t',instr.IF,'\t',instr.ID,'\t',
+    
+    for instr in i_list:
+        print instr.i,'\t',instr.IF,'\t',instr.ID,'\t',
 
-		if instr.EX:
-			if len(instr.EX) == 3:
-				print "%s/%s-%s"%(instr.EX[2],instr.EX[0],instr.EX[1]),
-			elif instr.EX[0] == instr.EX[1]:
-				print instr.EX[0],
-			else:
-				print "%s-%s"%instr.EX,
+        if instr.EX:
+            print instr.EX,
 
-		if instr.MEM:
-			print '\t',instr.MEM,
+        if instr.MEM:
+            print '\t',instr.MEM,
 
-		if instr.CM:
-			print '\t',instr.CM
-		else:
-			print
+        if instr.CM:
+            print '\t',instr.CM,
+        
+        print
 
 # Find dependencies and return delays if any
 def find_dependencies(current_instr,instr_seen):
+    # return [instr for instr in instr_seen if instr.ro in current_instr.rs]
+    result = []
+    for instr in instr_seen:
+        if instr.ro and instr.ro in current_instr.rs:
+            result.append(instr)
+    return result
 
-	# result = [instr for instr in instr_seen if instr.ro in current_instr.rs]
-	result = []
-	for instr in instr_seen:
-		if instr.ro and instr.ro in current_instr.rs:
-			result.append(instr)
-	return result
+def find_mem_dep(instr,instr_seen):
+    max = 0
+    for i in instr_seen:
+        if i.ro and i.ro in instr.r_mem and i.MEM.start > instr.MEM.start:
+            if i.MEM.start > max:
+                max = i.MEM.start
+    return max
 
 
 def fill_pipeline(i_list):
-	pc         = 0
-	clk        = 1
-	instr_seen = []
-	
-	while pc < len(i_list):
-		# Check for dependencies
-		dependency = find_dependencies(i_list[pc],instr_seen)		
-		instr_seen.append(i_list[pc])
+    pc         = 0
+    clk        = 1
+    instr_seen = []
+    stall      = None
+    bubble     = None
+    
+    while pc < len(i_list):
+        # Check for dependencies
+        dependency = find_dependencies(i_list[pc],instr_seen)
+        instr_seen.append(i_list[pc])
 
-		i_list[pc].IF   = clk
-		i_list[pc].ID   = clk + 1
-		i_list[pc].EX   = (i_list[pc].ID + 1,i_list[pc].ID + i_list[pc].latency)
+        # Handle IF
+        if bubble == '_IF':
+            clk     = i_list[pc-1].ID.start
+            bubble  = None
 
-		# Handle EX
-		if dependency:
-			tmp = []
-			for instr in dependency:
-				if instr.opcode in ['LD','L.D']	and instr.CM > i_list[pc].EX[0]:
-					tmp.append((instr.CM,
-											instr.CM + i_list[pc].latency,
-											i_list[pc].EX[0]))
-				if instr.opcode in ['MUL.D','ADD.D','SUB.D','DIV.D','DADDI'] \
-					and instr.MEM > i_list[pc].EX[0]:
-					tmp.append((instr.MEM,
-											instr.MEM + i_list[pc].latency,
-											i_list[pc].EX[0]))        
-			# Replace with itemgetter from operator
-			if tmp:
-				i_list[pc].EX = max(tmp, key = lambda item:item[0])
-				stall = 'EX'
-		
-		i_list[pc].MEM  = i_list[pc].EX[1] + 1
+        i_list[pc].IF.start = clk 
+            
+        if stall == 'IF':
+            i_list[pc].IF.stall   = i_list[pc-1].ID.stall
+            bubble                = '_IF'   # Final bubble
+            stall                 = None
 
-		# Handle SD/S.D
-		if i_list[pc].opcode in ['SD','S.D']:
-			i_list[pc].CM = None
-		else:
-			i_list[pc].CM = i_list[pc].MEM + 1
+        # Handle ID
+        i_list[pc].ID.start = (i_list[pc].IF.stall if i_list[pc].IF.stall else i_list[pc].IF.start)+1
+        if stall == 'ID':
+             i_list[pc].ID.stall  = i_list[pc-1].EX.stall
+             stall                = 'IF'
+                    
+        i_list[pc].EX.start = (i_list[pc].ID.stall if i_list[pc].ID.stall else i_list[pc].ID.start)+1
 
-		# Handle BNEZ
-		if i_list[pc].opcode == 'BNEZ':
-			i_list[pc].EX = i_list[pc].MEM = i_list[pc].CM = None 
+        # Handle EX
+        if dependency and i_list[pc].opcode != 'S.D':
+            max_stall = 0
+            for instr in dependency:
+                if instr.opcode in ['LD','L.D'] and instr.CM.start >= i_list[pc].EX.start:
+                    if instr.CM.start > max_stall:
+                        max_stall = instr.CM.start
+                if instr.opcode in ['MUL.D','ADD.D','SUB.D','DIV.D','DADDI']:
+                    if instr.MEM.start >= i_list[pc].EX.start:
+                        if instr.MEM.start > max_stall:
+                            max_stall = instr.MEM.start
+                    elif instr.CM.start >= i_list[pc].EX.start:
+                        if instr.CM.start > max_stall:
+                            max_stall = instr.CM.start                    
+            # Replace with itemgetter from operator
+            if max_stall and max_stall != i_list[pc].EX.start:
+                i_list[pc].EX.stall = max_stall
+                stall               = 'ID'
+        
+        i_list[pc].EX.end = (i_list[pc].EX.stall if i_list[pc].EX.stall else i_list[pc].EX.start) + i_list[pc].latency - 1
 
-		clk += 1
-		pc += 1
+        # Handle MEM
+        i_list[pc].MEM.start  = i_list[pc].EX.end + 1
+        if i_list[pc].opcode == 'S.D':
+            i_list[pc].MEM.start  = find_mem_dep(i_list[pc],instr_seen)
+            i_list[pc].EX.start = i_list[pc].MEM.start - 1
+            i_list[pc].EX.end = i_list[pc].EX.start
+            i_list[pc].ID.stall = i_list[pc].EX.start - 1
+
+        # Handle CM with SD/S.D
+        if i_list[pc].opcode in ['SD','S.D']:
+            i_list[pc].CM = None
+        else:
+            i_list[pc].CM.start = i_list[pc].MEM.start + 1
+
+        # Handle BNEZ
+        if i_list[pc].opcode == 'BNEZ':
+            i_list[pc].EX = i_list[pc].MEM = i_list[pc].CM = None 
+
+        clk += 1
+        pc += 1
 
 def main():
-	# Assumption, code is always valid
-	with open('input.txt','r') as f:
-		data = f.readlines()
-		
-	i_list = create_instructions(data)
-	fill_pipeline(i_list)
-	display(i_list)
-	
+    # Assumption, code is always valid
+    with open('input.txt','r') as f:
+        data = f.readlines()
+        
+    i_list = create_instructions(data)
+    fill_pipeline(i_list)
+    display(i_list)
+    
 
 # Entry
 main()
